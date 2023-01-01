f7ae311af64c5df09e4f7e9de9d17fda54589f6c
jessanahi <jess.anahi.zamora@gmail.com>
2023-01-01 15:56:08 -0600

    header logo down! i think.

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 function Header() {
   return (
     <header className={styles.header}>
-      <img alt='site logo' src={'https://res.cloudinary.com/teepublic/image/private/s--8wqKRZQj--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1482755311/production/designs/991823_1.jpg'} />
+      <img alt='site logo' src={'https://www.g33kmania.com/wp-content/uploads/stranger-things-logo-square.jpeg'} />
     </header>
   );
 }
